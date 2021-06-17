158fff7f7dcc560b9102b2916fbdc15f1b1d8d1a
Vista Tenasa <vistatenasaad@gmail.com>
2021-06-17 18:14:24 +0700

    Service

---
+++ lib/service/service.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:tontonin/models/movie.dart';
+
+class TontoninService {
+  final String apiKey = 'api_key=d95b18e60af5856565a0ba3a9c635a50';
+  final String baseUrl = 'https://api.themoviedb.org/3';
+
+  Future<List> getPopularMovies() async {
+    final String uri = baseUrl + '/movie/popular?' + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if (result.statusCode == HttpStatus.ok) {
+      print("Sukses");
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      print("Fail");
+      return null;
+    }
+  }
+
+  Future<List> getNowPlaying() async {
+    final String uri = baseUrl + '/movie/now_playing?' + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if (result.statusCode == HttpStatus.ok) {
+      print("Sukses");
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      print("Fail");
+      return null;
+    }
+  }
+
+  Future<List> getUpcoming() async {
+    final String uri = baseUrl + '/movie/upcoming?' + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if (result.statusCode == HttpStatus.ok) {
+      print("Sukses");
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      print("Fail");
+      return null;
+    }
+  }
+}
